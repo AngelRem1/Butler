89f750d0ee4372e712f87f932927a1be1970b8cc
AngelRem1 <51797591+AngelRem1@users.noreply.github.com>
2019-07-03 07:35:45 -0700

    Add files via upload

---
+++ 2main.py
@@ -0,0 +1,15 @@
+def main() :
+    print('Hello what is your name?')
+user_name = input()
+print('Welcome', user_name)
+print('May I take your order?')
+order = input()
+print(order, 'Is this your order? Type yes or no')
+confirmation = input()
+def start():
+    while True:
+        if confirmation == "yes":
+            print('I will be back with your order')
+
+        elif op != "yes":
+           print('I will try again')
